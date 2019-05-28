76facbf599bea7ebbc59c20df59e7ebc151b35c7
ntrung95 <trung.ng.ueh@gmail.com>
2019-05-29 00:14:46 +0700

    first function

--- far_cry.py
+++ far_cry.py
@@ -12,15 +12,23 @@ def parse_arguments():
 	return args
 
 
+def read_log_file(log_file_pathname):
+	return os.path.getsize(log_file_pathname)
+
+
+
 def main():
 	argument = parse_arguments()
 	if argument.log and not os.path.isfile(argument.log):
 		print('No fucking file homie')
 		exit(1)
 	file = argument.log
-
-
-
+	print(type(file), file)
+	# with open(file, 'r') as file_log:
+	# 	lines = file_log.readlines()
+	# for x in lines:
+	# 	print(x[:-1])
+	print(read_log_file(file))
 
 if __name__ == '__main__':
 	main()
