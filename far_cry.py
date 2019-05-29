c4d9f3968f523606a9da2652a42e6c2c220af4c7
gohan8895 <trung.ng.ueh@gmail.com>
2019-05-29 19:23:58 +0700

    adding frag list" sadasd exit dasdasd dasd exit() git adsad

--- far_cry.py
+++ far_cry.py
@@ -3,7 +3,7 @@
 import argparse
 import os
 import datetime
-
+import re
 
 def parse_arguments():
 	parser = 	argparse.ArgumentParser()
@@ -50,27 +50,50 @@ def parse_session_mode_and_map(log_data):
 			return (mode, map)
 
 def parse_frags(log_data):
+	'''
+	(frag_time, killer_name, victim_name, weapon_code)
+	(frag_time, killer_name)
+	list of tuples of strings
+	'''
 	log_lines = log_data.split('\n')
-	pass
+	list_frags = list()
+	for line in log_lines:
+		if 'killed' in line:
+			if len(line.split(' ')) == 5:
+				frag_time = line.split(' ')[0][1:6]
+				killer_name = line.split(' ')[2]
+				list_frags.append((frag_time, killer_name))
+			elif len(line.split(' ')) == 7:
+				frag_time = line.split(' ')[0][1:6]
+				killer_name = line.split(' ')[2]
+				victim_name = line.split(' ')[4]
+				weapon_code = line.split(' ')[6]
+				list_frags.append((frag_time, killer_name, victim_name, weapon_code))
+	return list_frags
+
+
 
 def main():
 	argument = parse_arguments()
 	if argument.log and not os.path.isfile(argument.log):
-		print('No fucking file homie')
+		print('File not found')
 		exit(1)
 	file_log = argument.log
 	log_data = read_log_file(file_log)
-	# print(len(log_data))
+	# print(log_data)
+	print(len(log_data))
 	log_lines = log_data.split('\n')
 	# for x in log_lines:
 	# 	print(x)
 	log_start_time = parse_log_start_time(log_data)
 	# print(log_start_time)
-	# print(log_start_time.isoformat())
+	print(log_start_time)
+	print(log_start_time.isoformat())
 	console_dict = create_console_variables_dict(log_data)
 	# for x, y in console_dict.items():
 	# 	print(x, y)
 	print(parse_session_mode_and_map(log_data))
+	list_frags = parse_frags(log_data)
 
 
 if __name__ == '__main__':
